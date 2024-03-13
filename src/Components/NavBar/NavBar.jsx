15cf18a8d9a916d45752a0e60a0892f1fd327c12
ArevaloJoseLuis <145626236+ArevaloJoseLuis@users.noreply.github.com>
2024-03-13 10:48:18 -0300

    Add files via upload

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import CartWidget from '../CartWidget/CartWidget';
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto navbar-expand-lg">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <div><CartWidget/></div> 
+        </Container>
+      </Navbar>
+      <br/>
+      
+     
+    </>
+  );
+}
+
+export default NavBar;
